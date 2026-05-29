d7745decabcdbc3178d426d860ef7f86354d5ed4
Tomy Acapozzo <tomyacapozzo@gmail.com>
2026-05-29 15:12:35 -0300

    fix: replace react-navigation HapticTab with Pressable for SDK 56

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,14 +1,12 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import { Pressable } from 'react-native';
 
-export function HapticTab(props: BottomTabBarButtonProps) {
+export function HapticTab(props: React.ComponentProps<typeof Pressable>) {
   return (
-    <PlatformPressable
+    <Pressable
       {...props}
       onPressIn={(ev) => {
         if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
         props.onPressIn?.(ev);
